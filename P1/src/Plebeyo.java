a9a284a085f047076068cee06b6de9a1152110a9
Pablo <pab23@gcloud.ua.es>
2018-04-05 11:49:55 +0200

    1.1 Empezando plebeyo

--- P1/src/Plebeyo.java
+++ P1/src/Plebeyo.java
@@ -11,12 +11,25 @@ public class Plebeyo extends Habitante {
         patrono=null;
         tribu=null;
     }
-    public ArrayList<String> trueue(Habitante h){
+    public ArrayList<String> trueque(Habitante h){
+        ArrayList<String> devuelve=new ArrayList<String>();
+        boolean encontrado=false;
+        int tipopas, i;
         if(h!=null) {
             if (this.getClan() == null && h.getClan() == null) {
                 super.trueque(h);
             }else if((this.getClan()==null && h.getClan()!=null)||(this.getClan()!=null && h.getClan()==null)){
-
+                if(this.getCesta().get(0)!=null){
+                    ArrayList<Producto> cesto=this.getCesta();
+                    for(i=1;i<cesto.size() && encontrado==false;i++){
+                        if(cesto.get(i).getTipo()!=cesto.get(0).getTipo()){
+                            tipopas=cesto.get(i).getTipo();
+                            encontrado=true;
+                        }
+                    }
+                    if(encontrado){
+                        haceTrueque(cesto.get(i).getTipo(), cesto.get(0));
+                    }
             }else if(h){
                 if (this.getClan().equals(h.getClan()) && this.bestiola != null && h.bestiola != null) {
 
